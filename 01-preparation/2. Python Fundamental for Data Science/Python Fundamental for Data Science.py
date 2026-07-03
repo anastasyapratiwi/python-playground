c7fdc5ccf34b7bc04a385cd78841e63f0cdd640c
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-03 20:45:57 +0700

    Implement CSV reading and bar chart visualization
    Added CSV reading and bar chart plotting using pandas and matplotlib.

--- 01-preparation/2. Python Fundamental for Data Science/Python Fundamental for Data Science.py
+++ 01-preparation/2. Python Fundamental for Data Science/Python Fundamental for Data Science.py
@@ -291,3 +291,72 @@ with closing(requests.get(url, stream=True)) as r:
         print(row)
 
 # Tugas Praktik
+import csv
+
+# tentukan lokasi file, nama file, dan inisialisasi csv
+f = open('https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv', 'r')
+reader = csv.reader(f)
+
+# membaca baris per baris
+for row in reader:
+	print(row)
+
+# menutup file csv
+f.close()
+
+# --- MEMBACA FILE CSV DENGAN MENGGUNAKAN PANDAS
+import pandas as pd
+pd.set_option("display.max_columns", 50)
+
+table = pd.read_csv(
+    "https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv"
+)
+table.head()
+print(table)
+
+# --- BAR CHART ---
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv(
+    "https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv"
+)
+table.head()
+x_label = table["NAMA KELURAHAN"]
+plt.bar(x=np.arange(len(x_label)), height=table["LAKI-LAKI WNI"])
+plt.show()
+
+# PARAMETER DALAM GRAFIK (Memberikan Nilai Axis dari data CSV)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv(
+    "https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv"
+)
+table.head()
+
+x_label = table["NAMA KELURAHAN"]
+plt.bar(x=np.arange(len(x_label)), height=table["LAKI-LAKI WNI"])
+plt.xticks(np.arange(len(x_label)), table["NAMA KELURAHAN"], rotation=30)
+plt.show()
+
+# --- MENAMBAH TITLE DAN LABEL PADA GRAFIK ---
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv(
+    "https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv"
+)
+table.head()
+
+x_label = table["NAMA KELURAHAN"]
+plt.bar(x=np.arange(len(x_label)), height=table["LAKI-LAKI WNI"])
+plt.xticks(np.arange(len(x_label)), table["NAMA KELURAHAN"], rotation=90)
+plt.xlabel("Kelurahan di Jakarta Pusat")
+plt.ylabel("Jumlah Penduduk Laki-Laki")
+plt.title("Persebaran Jumlah Penduduk Laki-Laki di Jakarta Pusat")
+
+plt.show()
