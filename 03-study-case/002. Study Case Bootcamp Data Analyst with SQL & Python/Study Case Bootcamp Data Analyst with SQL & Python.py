7daf634d12a6a382638e8d8087a9dd41cd851316
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-03 21:32:20 +0700

    Load dataset and split Customer_Name into First and Last names
    Added code to load dataset and split customer names.

--- 03-study-case/002. Study Case Bootcamp Data Analyst with SQL & Python/Study Case Bootcamp Data Analyst with SQL & Python.py
+++ 03-study-case/002. Study Case Bootcamp Data Analyst with SQL & Python/Study Case Bootcamp Data Analyst with SQL & Python.py
@@ -1,1 +1,13 @@
+# --- Profile Pemateri: Xeratic ---
 
+import pandas as pd
+
+# load dataset
+dataset = 'https://dqlabcdn.xeratic.com/dqlab-dataset/SuperStore.csv'
+df = pd.read_csv(dataset);
+
+# Pisahkan Customer Name menjadi dua komponen yaitu First_Name dan Last_Name
+df[['First_Name', 'Last_Name']] = df['Customer_Name'].str.split(' ', n=1, expand=True)
+
+# tampilkan 5 baris pertama
+print(df.head())
