e3b21b84242f89b0c5b360aeaf2debb616a96ffb
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-03 21:36:37 +0700

    Implement data loading and cleaning for housing dataset
    Added data loading and cleaning steps to the script.

--- 03-study-case/003. Study Case Bootcamp Machine Learning & AI for Beginner/Study Case Bootcamp Machine Learning & AI for Beginner.py
+++ 03-study-case/003. Study Case Bootcamp Machine Learning & AI for Beginner/Study Case Bootcamp Machine Learning & AI for Beginner.py
@@ -1,1 +1,18 @@
+# --- Profile Pemateri: Xeratic ---
 
+import pandas as pd
+
+# Load Dataset
+df = pd.read_csv('https://dqlabcdn.xeratic.com/dqlab-dataset/HousingData.csv')
+
+# Tampilkan jumlah data duplikat
+print(df.duplicated().sum())
+
+# Tampilkan jumlah data yang missing per kolom
+print(df.isna().sum())
+
+# Drop baris yang mempunyai baris kosong
+df = df.dropna()
+
+# Tampilkan berapa baris dan kolom sisa dari data yang sudah didrop
+print(df.shape)
