2bca334b04f8cb03bcde2a63a7ca1573fa9b37c0
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-03 21:05:18 +0700

    Add introductory content and examples for Python

--- 01-preparation/1. Introduction to Data Science with Python/Introduction to Data Science with Python.py
+++ 01-preparation/1. Introduction to Data Science with Python/Introduction to Data Science with Python.py
@@ -1,1 +1,39 @@
+# Profile Pemateri: XERATIC
 
+# -- Python “Hello World” & Statement --
+print("Hello World.")
+print("Saya Aksara, baru belajar Python.")
+
+# -- Variables di Python --
+bil1 = 10
+Bil_2 = 20
+Frasa = "Halo Dunia"
+bil1, Bil_2 = 10,20
+salam = "Selamat Pagi" ; Penutup = "Salam Sejahtera"
+
+# -- Comments di Python --
+# perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+# -- Tipe Data Python - Part 1 --
+print(int(5.3)) # Output: 5
+print(int("6")) # Output: 6
+
+# float()
+print(float(3)) # Output: 3.0
+print(float("9.23")) # Output: 9.23
+
+# Boolean --> bool()
+print(bool(1)) # Output: True
+print(bool(0)) # Output: False
+
+# -- Tipe Data Python - Part 2 --
+
+# -- Tugas Praktik --
+
+# -- Menggunakan Library di Python --
