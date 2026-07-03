b3aef7ea95ad99f351a1e8c64487ae263e3de22e
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-03 21:20:10 +0700

    Add data type examples in Python
    Added examples for str(), list(), tuple(), and set() functions.

--- 01-preparation/1. Introduction to Data Science with Python/Introduction to Data Science with Python.py
+++ 01-preparation/1. Introduction to Data Science with Python/Introduction to Data Science with Python.py
@@ -33,6 +33,24 @@ print(bool(1)) # Output: True
 print(bool(0)) # Output: False
 
 # -- Tipe Data Python - Part 2 --
+# str()
+print(str(5.3)) # Output: "5.3"
+print(str([1, 'buku'])) # Output: "[1, 'buku']"
+
+# list()
+print(list("buku")) # Output: ['b', 'u', 'k', 'u']
+print(list((1,2,3))) # Output: [1, 2, 3]
+print(list({1,3,6,3})) # Output: [1, 3, 6]
+
+# tuple()
+print(tuple("buku")) # Output: ('b', 'u', 'k', 'u')
+print(tuple((1, 2, 3))) # Output: (1, 2, 3)
+print(tuple({1,3,6,3})) # Output: (1, 3, 6)
+
+# set()
+print(set("buku")) # Output: {'k', 'b', 'u'}
+print(set((1,2,3))) # Output: {1, 2, 3}
+print(set([1,3,6,3])) # Output: {1, 3, 6}
 
 # -- Tugas Praktik --
 
