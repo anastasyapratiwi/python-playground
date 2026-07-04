6166d5a6951fe3fb0b6aa0121834e02e45ed8c0b
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-04 13:10:43 +0700

    Add file writing and math functions examples

--- 02-fundamental/01b. Python for Data Professional Beginner - Part 2/Python for Data Professional Beginner - Part 2.py
+++ 02-fundamental/01b. Python for Data Professional Beginner - Part 2/Python for Data Professional Beginner - Part 2.py
@@ -667,3 +667,224 @@ print(response)
 print("\n>> Cetak isi file hello.txt menggunakan atribut response.text:")
 print(response.text) 
 
+# --- Menulis Berkas Teks – Part 1 (w) ---
+# Menulis ke file hello.txt
+file = open("hello.txt", "w") # Python akan menghapus seluruh isi dalam berkas sebelum menuliskan konten yang aku spesifikasikan.
+file.write("Sekarang kita belajar menulis dengan menggunakan Python")
+file.write("Menulis konten file dengan mode w (write).")
+file.close()
+
+# --- Menulis Berkas Teks – Part 2 ---
+# Menulis ke file dengan mode append
+file = open("hello.txt", "a")
+file.writelines([
+"Sekarang kita belajar menulis dengan menggunakan Python", 
+"Menulis konten file dengan mode a (append)."
+])
+file.close()
+
+# --- Library Matematika pada Python ---
+# --- Fungsi dalam Library Matematika – Part 1 ---
+# Import library math
+import math
+
+# Fungsi math.ceil()
+print(">>> Fungsi math.ceil()")
+x = 10.32
+y = 13.87
+x_ceil = math.ceil(x)
+y_ceil = math.ceil(y)
+print(x_ceil)
+print(y_ceil)
+
+# Fungsi math.floor()
+print(">>> Fungsi math.floor()")
+x_floor = math.floor(x)
+y_floor = math.floor(y)
+print(x_floor)
+print(y_floor)
+
+# Fungsi math.fabs()
+print(">>> Fungsi math.fabs()")
+x = 10.32
+y = -13.87
+x = math.fabs(x)
+y = math.fabs(y)
+print(x)
+print(y)
+
+# Fungsi math.factorial()
+print(">>> Fungsi math.factorial()")
+x_factorial = math.factorial(5)
+print(x_factorial)
+
+# Fungsi math.fsum()
+print(">>> Fungsi math.fsum()")
+x = [1, 2, 3, 4, 5, 6, -6, -5, -4]
+total = math.fsum(x)
+print(total)
+
+# --- Fungsi dalam Library Matematika – Part 2 ---
+# Import library math
+import math
+
+# Fungsi math.log()
+print(">>> Fungsi math.log()")
+# x = log basis 2 dari 8
+x = math.log(8, 2)
+# y = log basis 3 dari 81
+y = math.log(81, 3)
+# z = log basis 10 dari 10000
+z = math.log(10000, 10) 
+print(x)
+print(y)
+print(z)
+
+# Fungsi math.sqrt()
+print(">>> Fungsi math.sqrt()")
+# akar kuadrat dari 100
+x = math.sqrt(100)
+print(x)
+# akar kuadrat dari 2
+y = math.sqrt(2)
+print(y)
+
+# Fungsi math.copysign()
+print(">>> Fungsi math.copysign()")
+x = 10.32
+y = -13.87
+z = -15
+x = math.copysign(x, z)
+y = math.copysign(y, z)
+z = math.copysign(z, 10)
+print(x)
+print(y)
+print(z)
+
+# --- Chapter 5: Mini Project ---
+import requests
+from contextlib import closing
+import csv
+
+# STEP 1: 
+# Baca file "harga_rumah.txt"
+url = "https://storage.googleapis.com/dqlab-dataset/harga_rumah.txt"
+data_harga_rumah = []
+with closing(requests.get(url, stream=True)) as r:
+    f = (line.decode('utf-8') for line in r.iter_lines())
+    data_harga_rumah = [row for row in csv.reader(f)]
+
+# Buat list of dict dengan nama harga rumah
+key_harga_rumah = data_harga_rumah[0]
+harga_rumah = []
+for baris_harga_rumah in data_harga_rumah[1:]:
+	dict_harga_rumah = dict()
+	for i in range(len(baris_harga_rumah)):
+		dict_harga_rumah[key_harga_rumah[i]] = baris_harga_rumah[i]
+	harga_rumah.append(dict_harga_rumah)
+print(harga_rumah)
+
+# STEP 2:
+# Buat fungsi  get_all_specified_attribute yang menerima parameter list_of_dictionary 
+# (tipe data list yang berisikan sekumpulan tipe data dictionary) dan specified_key 
+# (tipe data string). Fungsi akan mengembalikan sebuah list yang berisikan seluruh 
+# atribut dengan kunci (key) specified_key. 
+def get_all_specified_attributes(list_of_dictionary, specified_key):
+	list_attributes = []
+	for data in list_of_dictionary:
+		attribute = data[specified_key]
+		list_attributes.append(attribute)
+	return list_attributes
+
+# STEP 3: 
+# Buat fungsi fungsi min_value yang menerima parameter list_attributes (berupa 
+# tipe data list) dan mengembalikan nilai terkecil dalam list_attributes 
+def min_value(list_attributes):
+	min_attribute = 9999
+	for attr in list_attributes:
+		if int(attr) < min_attribute:
+			min_attribute = int(attr)
+	return min_attribute
+
+# Buat fungsi dan max_value yang menerima parameter list_attribute dan 
+# mengembalikan nilai terbesar dalam list_attributes.	
+def max_value(list_attributes):
+	max_attribute = -9999
+	for attr in list_attributes:
+		if int(attr) > max_attribute:
+			max_attribute = int(attr)
+	return max_attribute
+
+# STEP 4: 
+# Buat fungsi transform_attribute yang menerima parameter attr (sebuah 
+# bilangan), max_attr (sebuah bilangan) dan min_attr (sebuah bilangan) 
+# yang mengembalikan nilai transformasi dari sebuah attribute.
+def transform_attribute(attr, max_attr, min_attr):
+	nilai_transformasi = (attr - min_attr) / (max_attr - min_attr)
+	return nilai_transformasi
+
+# STEP 5:
+# Buat fungsi data_transformation yang menerima parameter list_of_dictionary 
+# (sebuah list yang berisikan tipe data dictionary) dan list_attribute_names 
+# (sebuah list yang berisikan tipe data string) mengembalikan hasil 
+# transformasi data dari list_of_dictionary berdasarkan list_attribute_names 
+# dan attr_info telah dispesifikasikan.
+def data_transformation(list_of_dictionary, list_attribute_names):
+	attr_info = {}
+	for attr_name in list_attribute_names:
+		specified_attributes = get_all_specified_attributes(list_of_dictionary, attr_name)
+		max_attr = max_value(specified_attributes)
+		min_attr = min_value(specified_attributes)
+		attr_info[attr_name] = {'max': max_attr, 'min': min_attr}
+		data_idx = 0
+		while(data_idx < len(list_of_dictionary)):
+			list_of_dictionary[data_idx][attr_name] = transform_attribute(int(list_of_dictionary[data_idx][attr_name]), max_attr, min_attr)
+			data_idx += 1
+	return list_of_dictionary, attr_info
+
+# STEP 6:
+# Berdasarkan data baru dan attr_info ini, buat fungsi transform_data yang
+# menerima parameter data dan attr_info dan mengembalikan nilai atribut 
+# dari data baru yang telah ditransformasikan.
+def transform_data(data, attr_info):
+	for key_name in data.keys():
+		data[key_name] = (data[key_name] - attr_info[key_name]['min']) / (
+		                  attr_info[key_name]['max'] - attr_info[key_name]['min'])
+	return data
+
+# STEP 7:
+# Buat fungsi yang digunakan untuk sistem prediksi harga berdasarkan 
+# nilai kemiripan atribut, yaitu argument input data dan list_of_data!
+def abs_value(value):
+	if value < 0:
+		return -value
+	else:
+		return value
+def price_based_on_similarity(data, list_of_data):
+	prediksi_harga = 0
+	perbedaan_terkecil = 999
+	for data_point in list_of_data:
+		perbedaan= abs_value(data['tanah'] - data_point['tanah'])
+		perbedaan+= abs_value(data['bangunan'] - data_point['bangunan'])
+		perbedaan+= abs_value(data['jarak_ke_pusat'] - data_point['jarak_ke_pusat'])
+		if perbedaan < perbedaan_terkecil:
+			prediksi_harga = data_point['harga']
+			perbedaan_terkecil = perbedaan
+	return prediksi_harga
+
+# STEP 8:
+# Hitung harga rumah yang telah ditransformasikan ke dalam variabel 
+# harga_rumah berikut dengan atributnya attr_info
+harga_rumah, attr_info = data_transformation(harga_rumah,
+                                             ['tanah','bangunan','jarak_ke_pusat'])
+
+# Gunakan variabel data untuk memprediksi harga rumah
+data = {'tanah': 110, 'bangunan': 80, 'jarak_ke_pusat': 35}
+
+# Transformasikan data tersebut dengan dengan menggunakan attr_info yang telah 
+# diperoleh yang kembali disimpan ke variabel data.
+data = transform_data(data, attr_info)
+
+# Hitunglah prediksi harga dari variabel data tersebut.
+harga = price_based_on_similarity(data, harga_rumah)
+print("Prediksi harga rumah: ", harga)
