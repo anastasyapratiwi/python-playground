09ffd91aef17c0fa03870928a6b708d26770dd40
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-04 12:52:49 +0700

    Enhance Python tutorial with data structure examples
    Added content covering various Python data structures and their manipulations, including lists, tuples, sets, and dictionaries. Included examples of functions and practical tasks related to data handling.

--- 02-fundamental/01b. Python for Data Professional Beginner - Part 2/Python for Data Professional Beginner - Part 2.py
+++ 02-fundamental/01b. Python for Data Professional Beginner - Part 2/Python for Data Professional Beginner - Part 2.py
@@ -1,1 +1,487 @@
+# Profil Pemateri: 
+'''
+Julio Christian Young
+Dosen dan Peneliti bidang Artificial Intelligence 
+Universitas Multimedia Nusantara
+'''
 
+# --- Chapter 1: Collections manipulation ---
+# --- Mengakses List dan Tuple – Part 1 ---
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+print(bulan_pembelian[0])
+print(bulan_pembelian[5])
+print(bulan_pembelian[-1])
+print(bulan_pembelian[-2])
+
+# --- Mengakses List dan Tuple – Part 2 ---
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+
+# output pertengahan_tahun Mei, Juni, Juli, Agustus (index 4 - 7)
+pertengahan_tahun = bulan_pembelian[4:8]
+print(pertengahan_tahun)
+
+# output awal_tahun Januari, Februari, Maret, April, Mei (index 0-4)
+awal_tahun = bulan_pembelian[:5]
+print(awal_tahun)
+
+# output akhir_tahun September, Oktober, November, Desember (index 8-akhir)
+akhir_tahun = bulan_pembelian[8:]
+print(akhir_tahun)
+
+# potong list/tuple (indeks negatif), output September, Oktober, November
+print(bulan_pembelian[-4:-1])
+
+# Penggabungan Dua atau Lebih List atau Tuple
+# menggunakan operator penambahan (+)
+
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
+
+# --- List Manipulation – Part 1 ---
+# Fitur .append()
+print(">>> Fitur .append()")
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan.append('Ketoprak')
+print(list_makanan)
+
+# Fitur .clear()
+print(">>> Fitur .clear()")
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan.clear()
+print(list_makanan)
+
+# Fitur .copy()
+print(">>> Fitur .copy()")
+list_makanan1 = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan2 = list_makanan1.copy()
+list_makanan3 = list_makanan1
+list_makanan2.append('Opor')
+list_makanan3.append('Ketoprak')
+print(list_makanan1)
+print(list_makanan2)
+
+# Fitur .count()
+print(">>> Fitur .count()")
+list_score = ['Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud']
+score_budi = list_score.count('Budi')
+score_sud = list_score.count('Sud')
+print(score_budi) # akan menampilkan output 4
+print(score_sud) # akan menampilkan output 3
+
+# Fitur .extend()
+print(">>> Fitur .extend()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu.extend(list_minuman)
+print(list_menu)
+
+# --- List Manipulation – Part 2 ---
+# Fitur .index()
+print(">>> Fitur .index()")
+list_score = ['Budi','Sud','Budi','Budi','Budi','Sud','Sud']
+score_pertama_sud = list_score.index('Sud') + 1
+print(score_pertama_sud) # akan menampilkan output 2
+
+# Fitur .insert()
+print(">>> Fitur .insert()")
+list_score = ['Budi','Sud','Budi','Budi','Sud']
+list_score.insert(3, 'Sud')
+print(list_score)
+
+# Fitur .pop()
+print(">>> Fitur .pop()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_menu.pop(1)
+print(list_menu)
+
+# Fitur .remove()
+print(">>> Fitur .remove()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.remove('Rendang')
+print(list_menu)
+
+# Fitur .reverse()
+print(">>> Fitur .reverse()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.reverse()
+print(list_menu)
+
+# Fitur .sort()
+print(">>> Fitur .sort()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.sort() # Default: Ascending
+print(list_menu) 
+list_menu.sort(reverse=True) # Descending
+print(list_menu) 
+
+# --- Tuple Manipulation ---
+# Fitur .count()
+print(">>> Fitur .count()")
+tuple_score = ('Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud')
+score_budi = tuple_score.count('Budi')
+score_sud = tuple_score.count('Sud')
+print(score_budi) # akan menampilkan output 4
+print(score_sud) # akan menampilkan output 3
+
+# Fitur .index()
+print(">>> Fitur .index()")
+tuple_score = ('Budi','Sud','Budi','Budi','Budi','Sud','Sud')
+score_pertama_sud = tuple_score.index('Sud')+1
+print(score_pertama_sud) # akan menampilkan output 2
+
+# --- Set Manipulation – Part 1 ---
+# Fitur .add()
+print(">>> Fitur .add()")
+set_buah = {'Jeruk','Apel','Anggur'}
+set_buah.add('Melon')
+print(set_buah)
+
+# Fitur .clear()
+print(">>> Fitur .clear()")
+set_buah = {'Jeruk','Apel','Anggur'}
+set_buah.clear()
+print(set_buah) # output = set() atau empty set{}
+
+# Fitur .copy()
+print(">>> Fitur .copy()")
+set_buah1 = {'Jeruk','Apel','Anggur'}
+set_buah2 = set_buah1
+set_buah3 = set_buah1.copy()
+set_buah2.add('Melon')
+set_buah3.add('Kiwi')
+print(set_buah1)
+print(set_buah2)
+
+# Fitur .update()
+print(">>> Fitur .update()")
+parcel1 = {'Anggur','Apel','Jeruk'}
+parcel2 = {'Apel','Kiwi','Melon'}
+parcel1.update(parcel2)
+print(parcel1)
+
+# Fitur .pop()
+print(">>> Fitur .pop()")
+parcel = {'Anggur','Apel','Jeruk'}
+buah = parcel.pop()
+print(buah)
+print(parcel)
+
+# Fitur .remove()
+print(">>> Fitur .remove()")
+parcel = {'Anggur','Apel','Jeruk'}
+parcel.remove('Apel')
+print(parcel)
+
+# --- Set Manipulation – Part 2 ---
+# Fitur .union()
+print(">>> Fitur .union()")
+parcel1 = {'Anggur','Apel','Jeruk'}
+parcel2 = {'Apel','Kiwi','Melon'}
+parcel3 = parcel1.union(parcel2)
+print(parcel1)
+print(parcel3)
+
+# Fitur .isdisjoint()
+print(">>> Fitur .isdisjoint()")
+parcel1 = {'Anggur','Apel','Jeruk'}
+parcel2 = {'Kiwi','Melon','Pisang'}
+parcel3 = {'Apel','Srikaya','Semangka'}
+parcel1_parcel2_disjoint = parcel1.isdisjoint(parcel2)
+print(parcel1_parcel2_disjoint)
+parcel1_parcel3_disjoint = parcel1.isdisjoint(parcel3)
+print(parcel1_parcel3_disjoint)
+
+# Fitur .issubset()
+print(">>> Fitur .issubset()")
+parcel_A = {'Anggur', 'Apel'}
+parcel_B = {'Durian','Semangka','Apel'}
+parcel_C = {'Anggur', 'Kiwi', 'Apel', 'Jeruk', 'Melon'}
+parcel_A_dalam_C = parcel_A.issubset(parcel_C)
+parcel_B_dalam_C = parcel_B.issubset(parcel_C)
+print(parcel_A_dalam_C)
+print(parcel_B_dalam_C)
+
+# Fitur .issuperset()
+print(">>> Fitur .issuperset()")
+parcel_C_mengandung_A = parcel_C.issuperset(parcel_A)
+parcel_C_mengandung_B = parcel_C.issuperset(parcel_B)
+print(parcel_C_mengandung_A)
+print(parcel_C_mengandung_B)
+
+# Fitur .intersection()
+print(">>> Fitur .intersection()")
+parcel_A = {'Anggur', 'Kiwi', 'Apel', 'Jeruk', 'Melon'}
+parcel_B = {'Apel', 'Jeruk', 'Semangka', 'Durian', 'Tomat'}
+parcel_C = parcel_A.intersection(parcel_B)
+print(parcel_C)
+
+# Fitur .difference()
+print(">>> Fitur .difference()")
+parcel_C = parcel_A.difference(parcel_B)
+print(parcel_C)
+
+# Fitur .symmetric_difference()
+print(">>> Fitur .symmetric_difference()")
+parcel_A = {'Anggur', 'Apel', 'Jeruk', 'Melon'}
+parcel_B = {'Apel','Jeruk','Semangka','Leci'}
+parcel_C = parcel_A.symmetric_difference(parcel_B)
+print(parcel_C)
+
+# --- Dictionary Manipulation ---
+# Fitur .clear()
+print(">>> Fitur .clear()")
+info_karyawan = {'nama' : 'Aksara',
+                 'nik' : '1211011',
+                 'pekerjaan' : 'Data Analyst'}
+info_karyawan.clear()
+print(info_karyawan)
+
+# Fitur .copy()
+print(">>> Fitur .copy()")
+info_karyawan1 = {'nama' : 'Aksara',
+                  'nik' : '1211011',
+                  'pekerjaan' : 'Data Analyst'}
+info_karyawan2 = info_karyawan1.copy()
+info_karyawan2['nama'] = 'Senja'
+info_karyawan2['nik'] = '1211056'
+print(info_karyawan1)
+print(info_karyawan2)
+
+
+# Fitur .keys()
+print(">>> Fitur .keys()")
+info_karyawan = {'nama' : 'Aksara',
+                 'nik' : '1211011',
+                 'pekerjaan' : 'Data Analyst'}
+kunci_akses = list(info_karyawan.keys())
+print(kunci_akses) # Outputnya bersifat iterable sehingga dapat digunakan dalam perulangan.
+
+# Fitur .values()
+print(">>> Fitur .values()")
+value_dict = list(info_karyawan.values())
+print(value_dict)
+
+# Fitur .update()
+print(">>> Fitur .update()")
+info_karyawan.update({'skillset':['Python', 'R']})
+print(info_karyawan)
+
+# --- Useful Tips and Tricks ---
+# --- Tugas 1 ---
+# Tuple
+print(">>> Tuple")
+tuple_menu = ('Gado-gado','Ayam Goreng','Rendang','Ketoprak')
+jumlah_menu = len(tuple_menu)
+print(jumlah_menu)
+
+# List
+print(">>> List")
+list_menu = ['Gado-gado','Ayam Goreng','Rendang','Ketoprak']
+jumlah_menu = len(list_menu)
+print(jumlah_menu)
+
+# --- Tugas 2 ---
+# Konversi tipe data
+print(">>> Konversi tipe data")
+list_buah = ['Apel', 'Apel', 'Jeruk', 'Markisa', 'Jeruk', 'Markisa', 'Apel']
+set_buah = set(list_buah)
+print(set_buah)
+list_buah = list(set_buah)
+list_buah.sort()
+print(list_buah)
+
+# Tugas Praktik
+# menghitung rata-rata pengeluaran dan pemasukanku selama 9 bulan terakhir
+
+# Data keuangan
+keuangan = {
+'pengeluaran': [2, 2.5, 2.25, 2.5, 3.2, 2.5, 3.5, 4, 3],
+'pemasukan': [7.8, 7.5, 9, 7.6, 7.2, 7.5, 7, 10, 7.5]
+}
+
+# Perhitungan rata-rata pemasukan dan rata-rata pengeluaran
+total_pengeluaran = 0
+total_pemasukan = 0
+for biaya in keuangan['pengeluaran']: 
+    total_pengeluaran += biaya # Menambah total_pengeluaran dengan tiap biaya
+for biaya in keuangan['pemasukan']: 
+    total_pemasukan += biaya 
+rata_rata_pengeluaran = total_pengeluaran / len(keuangan['pengeluaran']) 
+rata_rata_pemasukan = total_pemasukan / len(keuangan['pemasukan'])
+print(rata_rata_pengeluaran) 
+print(rata_rata_pemasukan)
+
+# --- Chapter 2: String Manipulation dengan Python ---
+# --- Tinjauan Ulang String pada Python ---
+nama_produk = 'Sepatu Niko'
+kategori_produk = "Busana Pria"
+
+# Tanda kutip satu (6 kali)
+deskripsi_produk = '''Sepatu Niko merupakan sepatu kasual yang aerodinamis.
+Sepatu ini tersedia dalam berbagai macam ukuran. Untuk
+informasi lebih lanjut hubungi kami di no. 0832-xxxx-xxxx.
+'''
+print(deskripsi_produk)
+
+# Tanda kutip dua (6 kali)
+komentar_terbaik_produk = """Sepatu Niko nyaman dipakai dan
+bahannya tidak mudah kotor.
+"""
+print(komentar_terbaik_produk)
+
+# Apa itu String Manipulation?
+nama_produk = "Sepatu Niko"
+nama_produk = nama_produk[:2] + "P" + nama_produk[3:9] + "K" + nama_produk[-1]
+print(nama_produk) # Output: SePatu NiKo
+print(nama_produk[:7]) # Output: SePatu
+print(nama_produk[7:]) # Output: NiKo
+print(len(nama_produk)) # Output: 11
+
+# Operator “+” untuk Tipe Data String
+nama_depan = 'John'
+nama_belakang = 'Doee'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+print(nama_lengkap) # Output: John Doee
+
+umur = '27 tahun'
+alamat = 'Jl. Anggrek No. 100'
+nama_umur_alamat = 'Hi, saya ' + nama_lengkap + ' umur ' + umur + ', tinggal di ' + alamat + '.'
+print(nama_umur_alamat) # Output: Hi, saya John Doee umur 27 tahun, tinggal di Jl. Anggrek No. 100.
+
+# --- Menghilangkan Spasi di Awal dan/atau di Akhir ---
+# Fitur .strip()
+print(">>> Fitur .strip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.strip()
+print(kata_sambutan) # Output: 'halo, selamat siang!'
+
+# Fitur .lstrip()
+print(">>> Fitur .lstrip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.lstrip()
+print(kata_sambutan) # Output: 'halo, selamat siang! '
+
+# Fitur .rstrip()
+print(">>> Fitur .rstrip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.rstrip()
+print(kata_sambutan) # Output: ' halo, selamat siang!'
+
+# --- Mengubah Caps pada String ---
+# Fitur .capitalize()
+print(">>> Fitur .capitalize()")
+judul_buku = 'belajar bahasa Python'
+print(judul_buku.capitalize()) # Output: Belajar bahasa python
+
+# Fitur .lower()
+print(">>> Fitur .lower()")
+judul_buku = 'Belajar Bahasa PYTHON.'
+print(judul_buku.lower()) # Output: belajar bahasa python.
+
+# Fitur .upper()
+print(">>> Fitur .upper()")
+judul_buku = 'Belajar Bahasa PYTHON.'
+print(judul_buku.upper()) # Output: BELAJAR BAHASA PYTHON.
+
+# --- Pemecahan, Penggabungan, dan Penggantian String ---
+# Fitur .split()
+print(">>> Fitur .split()")
+frasa = "ani dan budi dan wati dan johan"
+karakter = frasa.split("dan")
+print(karakter) # Output: ['ani', 'budi', 'wati', 'johan']
+
+kata = frasa.split(" ")
+print(kata) # Output: ['ani', 'dan', 'budi', 'dan', 'wati', 'dan', 'johan'] 
+
+# Fitur .join()
+print(">>> Fitur .join()")
+pemisah = " dan "
+karakter = ["Ricky", "Peter", "Jordan"]
+frasa = pemisah.join(karakter)
+print(frasa) # Output: 'Ricky dan Peter dan Jordan'
+
+frasa = " ".join(karakter)
+print(frasa) # Output: 'Ricky Peter Jordan'
+
+# Fitur .replace()
+print(">>> Fitur .replace()")
+frasa = "apel malang apel yang paling segar, apel sehat, apel nikmat"
+frasa = frasa.replace("apel", "jeruk")
+print(frasa) # Output: 'jeruk malang jeruk yang paling segar, jeruk sehat, jeruk nikmat'
+
+# --- Menentukan Posisi dan Jumlah Sub-string pada String ---
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+
+# Fitur .find()
+print(">>> Fitur .find()")
+print(teks.find("Apel")) # Output: 0 (dikarenakan kalimat diawali dengan kata Apel)
+print(teks.find("malang")) # Output: 5 (kata malang muncul dimulai dari indeks ke-5 ketika setiap karakter dalam string direpresentasikan sebagai array.)
+
+# Fitur .count()
+print(">>> Fitur .count()")
+kemunculan_kata_apel = teks.count("apel")
+print(kemunculan_kata_apel) # Output: 3 (Apel diawali huruf kapital dan kata yang dicari diawali huruf kecil)
+
+# --- Menentukan String Apakah Diawali/Diakhiri oleh Sub-string ---
+# Fitur .startswith()
+print(">>> Fitur .startswith()")
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+print(teks.startswith("Apel")) #True
+print(teks.startswith("apel")) #False
+
+# Fitur .endswith()
+print(">>> Fitur .endswith()")
+print(teks.endswith("lainnya")) #True
+print(teks.endswith("apel")) #False
+
+# --- Tugas Praktik (PENGOLAHAN DATA TEKS) ---
+judul_artikel = [
+"Buah Salak Baik untuk Mata", "Buah Salak Kaya Potasium", 
+"Buah Jeruk Kaya Vitamin C", "Buah Salak Kaya Manfaat", 
+"Salak Baik untuk Jantung", "Jeruk dapat Memperkuat Tulang", 
+"Jeruk Mencegah Penyakit Asma", "Jeruk Memperkuat Gigi", 
+"Jeruk Mencegah Kolesterol Jahat", "Salak Mencegah Diabetes", 
+"Salak Memperkuat Dinding Usus", "Salak Baik untuk Darah",
+"Jeruk Kaya Manfaat untuk Jantung", "Salak si Kecil yang Baik", 
+"Jeruk dan Salak Buah Kaya Manfaat", "Buah Jeruk Enak",
+"Tips Panen Jeruk Ribuan Kilo", "Tips Bertanam Salak", 
+"Salak Manis untuk Berbuka", "Jeruk Baik untuk Wajah"
+]
+jumlah_artikel_jeruk = 0
+jumlah_artikel_salak = 0
+for judul in judul_artikel:
+    if judul.count("Jeruk") > 0: 
+        jumlah_artikel_jeruk += 1
+    if judul.count("Salak") > 0:
+        jumlah_artikel_salak += 1
+print(jumlah_artikel_jeruk) 
+print(jumlah_artikel_salak)
+
+# --- Tugas Praktik (Kata Positif) ---
+judul_artikel = [
+"Buah Salak Baik untuk Mata", "Buah Salak Kaya Potasium", 
+"Buah Jeruk Kaya Vitamin C", "Buah Salak Kaya Manfaat", 
+"Salak Baik untuk Jantung", "Jeruk dapat Memperkuat Tulang", 
+"Jeruk Mencegah Penyakit Asma", "Jeruk Memperkuat Gigi", 
+"Jeruk Mencegah Kolesterol Jahat", "Salak Mencegah Diabetes", 
+"Salak Memperkuat Dinding Usus", "Salak Baik untuk Darah",
+"Jeruk Kaya Manfaat untuk Jantung", "Salak si Kecil yang Baik", 
+"Jeruk dan Salak Buah Kaya Manfaat", "Buah Jeruk Enak",
+"Tips Panen Jeruk Ribuan Kilo", "Tips Bertanam Salak", 
+"Salak Manis untuk Berbuka", "Jeruk Baik untuk Wajah"
+]
+kata_positif = ["Kaya", "Baik", "Mencegah", "Memperkuat"]
+kata_positif_jeruk = 0
+kata_positif_salak = 0
+for judul in judul_artikel: 
+    for kata in kata_positif:
+        if judul.count("Jeruk") > 0 and judul.count(kata) > 0: 
+            kata_positif_jeruk += 1
+        if judul.count("Salak") > 0 and judul.count(kata) > 0:
+            kata_positif_salak += 1
+print(kata_positif_jeruk) 
+print(kata_positif_salak)
+
+# --- Chapter 3: Functions ---
