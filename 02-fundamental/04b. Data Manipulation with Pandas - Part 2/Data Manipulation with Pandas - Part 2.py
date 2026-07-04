d42f3a04ba81e007a454be21d37875a9f0e63e28
Anastasya Pratiwi <anastasyaprtw@gmail.com>
2026-07-04 21:11:43 +0700

    Add data manipulation examples using Pandas

--- 02-fundamental/04b. Data Manipulation with Pandas - Part 2/Data Manipulation with Pandas - Part 2.py
+++ 02-fundamental/04b. Data Manipulation with Pandas - Part 2/Data Manipulation with Pandas - Part 2.py
@@ -1,1 +1,456 @@
+# Profil Pemateri: Karl Christian, Business Intelligence, Traveloka
 
+# --- Chapter 1: Penggabungan Series/Dataframe ---
+# --- Bagaimana Cara Menggabungkan Pandas Series/Dataframe? ---
+import pandas as pd
+
+# Buat series of int (s1) dan series of string (s2)
+s1 = pd.Series([1,2,3,4,5,6])
+s2 = pd.Series (["a","b","c","d","e","f"])
+
+# Terapkan method append
+s2_append_s1 = pd.concat([s2, s1])
+print("Series - append:\n", s2_append_s1)
+
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+		            'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+		            'a':[3,4]})
+
+# Terapkan method append
+df2_append_df1 = pd.concat([df2,df1])
+print("Dataframe - append:\n", df2_append_df1)
+
+# --- Concat ---
+import pandas as pd
+
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+					'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+					'a':[3,4]})
+
+# Terapkan method concat row-wise
+row_wise_concat = pd.concat([df2,df1])
+print("Row-wise - concat:\n", row_wise_concat)
+
+# Terapkan method concat column-wise
+col_wise_concat = pd.concat([df2,df1],axis=1)
+print("Column-wise - concat:\n", col_wise_concat)
+
+# Penambahan identifier --> membentuk hasil penggabungan multiindex
+multiindex_concat = pd.concat([df2,df1], axis=0, keys=['df2','df1'])
+print("Multiindex - concat:\n", multiindex_concat)
+
+# --- Merge - Part 1 ---
+# --- Tugas Praktik ---
+import pandas as pd
+
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+
+# Merge yang ekivalen dengan SQL left join
+merge_df_left = pd.merge(left=df2, right=df1, how='left', left_on='key', right_on='key')
+print('Merge - Left:\n', merge_df_left)
+
+# Merge yang ekivalen dengan SQL right join
+merge_df_right = pd.merge(left=df2, right=df1, how='right', left_on='key', right_on='key')
+print('Merge - Right:\n', merge_df_right)
+
+# Merge yang ekivalen dengan SQL inner join
+merge_df_inner = pd.merge(left=df2, right=df1, how='inner', left_on='key', right_on='key')
+print('Merge - Inner:\n', merge_df_inner)
+
+# Merge yang ekivalen dengan SQL outer join
+merge_df_outer = pd.merge(left=df2, right=df1, how='outer', left_on='key', right_on='key')
+print('Merge - Outer:\n', merge_df_outer)
+
+# --- Merge - Part 2 ---
+import pandas as pd
+
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+}).set_index(['key','val2'])
+print('Dataframe 1:\n', df1)
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+}).set_index(['key','val3'])
+print('Dataframe 2:\n', df2)
+
+# Merge dataframe yang memiliki multi index
+df_merge = pd.merge(df1.reset_index(),df2.reset_index())
+print('Merging dataframe:\n', df_merge)
+
+# --- Join ---
+import pandas as pd
+
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+
+# Penerapan join dengan menggunakan set_index dan keyword how
+join_df = df1.set_index('key').join(df2.set_index('key'), how='outer')
+print(join_df)
+
+# --- CHAPTER 2: Pivot, Melt, Stack & Unstack ---
+# --- Dataset ---
+# --- Tugas Praktek: ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+
+# Unique value pada setiap kolom data
+for column in data.columns:
+    print('Unique value %s: %s' % (column, data[column].unique))
+
+# --- Pivot ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+
+# Pivoting with single column measurement
+pivot1 = data.pivot(index='murid',columns='pelajaran',values='nilai')
+print('Pivoting with single column measurement:\n', pivot1)
+
+# Pivoting with multiple column measurement
+pivot2 = data.pivot(index='murid',columns='pelajaran')
+print('Pivoting with multiple column measurement:\n', pivot2)
+
+# --- Pivot_table ---
+# --- Tugas Praktek: ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+
+# Creating pivot and assign pivot_tab dengan menggunakan keyword aggfunc='mean'
+pivot_tab_mean = data.pivot_table(index='kelas',columns='pelajaran',values='nilai',aggfunc='mean')
+print('Creating pivot table -- aggfunc mean:\n', pivot_tab_mean)
+
+# Creating pivot and assign pivot_tab dengan menggunakan keyword aggfunc='median'
+pivot_tab_median = data.pivot_table(index='kelas',columns='pelajaran',values='nilai',aggfunc='median')
+print('Creating pivot table -- aggfunc median:\n', pivot_tab_median)
+
+# --- Melt - Part 1 ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+
+# Pivoting dataframe
+data_pivot = data.pivot_table(index='kelas',columns='pelajaran', values='nilai',aggfunc='mean').reset_index()
+print('Pivoting dataframe:\n', data_pivot)
+
+# [1] Melting dataframe data_pivot
+data_melt_1 = pd.melt(data_pivot)
+print('Melting dataframe:\n', data_melt_1)
+
+# [2] Melting dataframe data_pivot dengan id_vars
+data_melt_2 = pd.melt(data_pivot, id_vars='kelas')
+print('Melting dataframe dengan idvars:\n', data_melt_2)
+
+# --- Melt - Part 2 ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+
+# Pivoting dataframe
+data_pivot = data.pivot_table(index='kelas',columns='pelajaran',values='nilai', aggfunc='mean').reset_index()
+print('Pivoting dataframe:\n', data_pivot)
+
+# [3.a] Melting dataframe data_pivot dengan value_vars
+data_melt_3a = pd.melt(data_pivot, value_vars=['math'])
+print('Melting dataframe dengan value_vars:\n', data_melt_3a)
+
+# [3.b] Melting dataframe data_pivot dengan id_vars dan value_vars
+data_melt_3b = pd.melt(data_pivot, id_vars='kelas', value_vars=['math'])
+print('Melting dataframe dengan id_vars dan value_vars:\n', data_melt_3b)
+
+# [4] Melting dataframe data_pivot dengan id_vars, value_vars, var_name. dan value_name
+data_melt_4 = pd.melt(data_pivot, id_vars='kelas', value_vars=['english','math'], var_name='pelajaran',value_name='nilai')
+print('Melting dataframe dengan id_vars, value_vars, var_name. dan value_name:\n', data_melt_4)
+
+# --- Stack & Unstack - Part 1 ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+print('Dataframe:\n', data)
+
+# Set index data untuk kolom kelas, murid, dan pelajaran
+data = data.set_index(['kelas','murid','pelajaran'])
+print('Dataframe multi index:\n', data)
+
+# [1] Unstacking dataframe
+data_unstack_1 = data.unstack()
+print('Unstacking dataframe:\n', data_unstack_1)
+
+# [2] Unstacking dengan specify level name
+data_unstack_2 = data.unstack(level='murid')
+print('Unstacking dataframe dengan level name:\n', data_unstack_2)
+
+# [3] Unstacking dengan specify level position
+data_unstack_3 = data.unstack(level=1)
+print('Unstacking dataframe dengan level position:\n', data_unstack_3)
+
+# --- Stack & Unstack - Part 2 ---
+import pandas as pd
+
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+data = data.set_index(['kelas','murid','pelajaran'])
+data_unstack = data.unstack(level=1)
+print('Dataframe:\n', data_unstack)
+
+# [1] Stacking dataframe
+data_stack = data_unstack.stack()
+print('Stacked dataframe:\n', data_stack)
+
+# [2] Tukar posisi index setelah stacking dataframe
+data_swap = data_stack.swaplevel(1,2)
+print('Swapped data:\n', data_swap)
+
+# [3] Melakukan sort_index pada stacking dataframe
+data_sort = data_swap.sort_index()
+print('Sorted data:\n', data_sort)
+
+# --- CHAPTER 3: Aggregation & GroupBy ---
+# --- Review Inspeksi Data ---
+import pandas as pd
+
+# Load data global_air_quality.csv
+global_air_quality = pd.read_csv('https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+print('Lima data teratas:\n', global_air_quality.head())
+
+# Melakukan pengecekan terhadap data
+print('Info global_air_quality:\n', global_air_quality.info())
+
+# Melakukan count tanpa groupby
+print('Count tanpa groupby:\n', global_air_quality.count())
+
+# Melakukan count dengan groupby 
+gaq_groupby_count = global_air_quality.groupby('source_name').count()
+print('Count dengan groupby (5 data teratas):\n', gaq_groupby_count.head())
+
+# --- Groupby dan Aggregasi dengan Fungsi Statistik Dasar - Part 1 ---
+import pandas as pd
+
+# Load data global_air_quality.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+
+# Create variabel pollutant
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+
+# [1] Group berdasarkan country dan terapkan aggregasi mean
+pollutant_mean = pollutant.groupby('country').mean()
+print('Rata-rata pollutant (5 teratas):\n', pollutant_mean.head())
+
+# [2] Group berdasarkan country dan terapkan aggregasi std
+pollutant_std = pollutant.groupby('country').std().fillna(0)
+print('Standar deviasi pollutant (5 teratas):\n', pollutant_std.head())
+
+# --- Groupby dan Aggregasi dengan Fungsi Statistik Dasar - Part 2 ---
+import pandas as pd
+
+# Load data https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+
+# Create variabel pollutant
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+
+# [3] Group berdasarkan country dan terapkan aggregasi sum
+pollutant_sum = pollutant.groupby('country').sum()
+print('Total pollutant (5 teratas):\n', pollutant_sum.head())
+
+# [4] Group berdasarkan country dan terapkan aggregasi nunique
+pollutant_nunique = pollutant.groupby('country').nunique()
+print('Jumlah unique value pollutant (5 teratas):\n', pollutant_nunique.head())
+
+# --- Groupby dan Aggregasi dengan Fungsi Statistik Dasar - Part 3 ---
+# --- Tugas Praktik: ---
+import pandas as pd
+
+# Load data https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+
+# Create variabel pollutant
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+
+# Group berdasarkan country dan terapkan aggregasi first
+pollutant_first = pollutant.groupby('country').first()
+print('Item pertama pollutant (5 teratas):\n', pollutant_first.head())
+
+# Group berdasarkan country dan terapkan aggregasi last
+pollutant_last = pollutant.groupby('country').last()
+print('Item terakhir pollutant (5 teratas):\n', pollutant_last.head())
+
+# --- Groupby dengan Multiple Aggregations ---
+# --- Tugas Praktik: ---
+import pandas as pd
+
+# Load data https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+
+# Create variabel pollutant
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+
+# Group berdasarkan country dan terapkan aggregasi: min, median, mean, max
+multiagg = pollutant.groupby('country').agg(['min','median','mean','max'])
+print('Multiple aggregations (5 teratas):\n', multiagg.head())
+
+# --- Groupby dengan Custom Aggregations ---
+# --- Tugas Praktek: ---
+import pandas as pd
+
+# Load data https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+
+# Create variabel pollutant
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+
+# Create sebuah function: iqr
+def iqr(series):
+	Q1 = series.quantile(0.25)
+	Q3 = series.quantile(0.75)
+	return Q3-Q1
+
+# Group berdasarkan country dan terapkan aggregasi dari function: iqr
+custom_agg = pollutant.groupby('country').agg(iqr)
+print('Custom aggregation (5 teratas):\n', custom_agg.head())
+
+# --- Groupby dengan Custom Aggregations by dict ---
+# Tugas Praktik:
+import pandas as pd
+
+# Load data https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+
+# Create variabel pollutant
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+
+# Function IQR
+def iqr(series):
+	return series.quantile(0.75) - series.quantile(0.25)
+
+# Create custom aggregation using dict
+custom_agg_dict = pollutant['value'][['pm10','pm25','so2']].groupby('country').agg({
+   'pm10':'median',
+   'pm25':iqr,
+   'so2':iqr
+})
+print('\nCetak 5 data teratas custom_agg_dict:\n', custom_agg_dict.head())
+
+# --- CHAPTER 4: Time Series in Pandas ---
+# --- Load Dataset as Time Series ---
+import pandas as pd
+
+# Load dataset https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv", parse_dates=True, index_col='timestamp')
+
+# Cetak 5 data teratas
+print(gaq.head())
+
+# Cetak info dari dataframe gaq
+print('info')
+print(gaq.info())
+
+# --- Convert to Datetime ---
+import pandas as pd
+
+# Load dataset https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+
+# Cetak 5 data teratas
+print('Sebelum diubah dalam format datetime:\n', gaq.head())
+
+# Ubah menjadi datetime
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+
+# Cetak 5 data teratas
+print('Sesudah diubah dalam format datetime:\n', gaq.head())
+
+# --- Downsampling Data ---
+import pandas as pd
+
+# Load dataset https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv("https://dqlabcdn.xeratic.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv")
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+print('Dataset sebelum di-downsampling (5 teratas):\n', gaq.head())
+
+# [1] Downsampling dari daily to weekly dan kita hitung maksimum untuk seminggu
+gaq_weekly = gaq.resample('W').max()
+print('Downsampling daily to weekly - max (5 teratas):\n', gaq_weekly.head())
+
+# [2] Downsampling dari daily to quarterly dan kita hitung minimumnya untuk tiap quarter
+gaq_quarterly = gaq.resample('Q').min()
+print('Downsampling daily to quarterly - min (5 teratas):\n', gaq_quarterly.head())
+
+# --- Upsampling Data ---
