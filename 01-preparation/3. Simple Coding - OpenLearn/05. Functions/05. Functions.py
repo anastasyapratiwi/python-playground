098cd036872ac688f9340b28c95eb3a641d307b1
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-09 11:13:45 +0700

    Add function examples and coding tips in Functions.py
    Added examples of sum, length, and input functions with comments.

--- 01-preparation/3. Simple Coding - OpenLearn/05. Functions/05. Functions.py
+++ 01-preparation/3. Simple Coding - OpenLearn/05. Functions/05. Functions.py
@@ -1,1 +1,37 @@
+"""
+Simple Coding - OpenLearn
+Chapter 05. Functions
 
+Instructor:
+Michel Wermelinger
+Faculty of Mathematics, Computing and Technology
+Open University
+"""
+
+# The sum function
+items = [4.35, 2, 19.95, 22.70, 5]
+expenses = sum(items)
+print("Food and drinks:"), expenses
+
+# The length function
+items = [4.35, 2, 19.95, 22.70, 5]
+expenses = len(items)
+print("Food and drinks:"), expenses
+
+# -- 5.1 The input function --
+# a. Input string
+name = input("Tasya")
+print("Nice to meet you,"), name
+
+# b. Input number
+year = input(2001)
+print("Your age is"), 2026 - year
+
+# -- 5.2 Conversion function --
+
+# -- Tips from Mr. Wermelinger --
+"""
+1. Good programmers don’t just write code, they write readable code.
+2. Code always changes to accommodate further customer requests.
+3. Using comments and descriptive names for variables and functions helps making code readable.
+"""
