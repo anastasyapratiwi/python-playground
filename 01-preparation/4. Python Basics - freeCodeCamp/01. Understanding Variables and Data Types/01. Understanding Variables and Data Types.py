1f857840a32d77cebb9ccbe3d88b767184c8c0ea
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-10 10:42:36 +0700

    Starting my journey in freeCodeCamp Python Certification
    Starts with the chapter: Python Basics. Wish me luck!

---
+++ 01-preparation/4. Python Basics - freeCodeCamp/01. Understanding Variables and Data Types/01. Understanding Variables and Data Types.py
@@ -0,0 +1,25 @@
+"""
+Python Certification freeCodeCamp
+Chapter: Python Basics
+Sub-Chapter: Understanding Variables and Data Types
+"""
+
+# -- A. How Do You Declare Variables and What Are Naming Conventions to Name Variables? --
+# 1. Example to create name and age variables:
+name = 'John Doe'
+age = 25
+
+# 2. Variable names should be in lowercase, separated by an underscore (snake case)
+my_variable_name = 'freeCodeCamp'
+
+# 3. Use descriptive names for variables
+user_age = 30
+
+# 4. Avoid using single-letter variable names
+x = 56 # What do you mean by x?
+
+# 5. Comments to let you add notes and explanations to your code
+# This is a single-line comment
+# This is a
+# multi-line
+# comment
