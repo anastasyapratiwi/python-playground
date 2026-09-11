2bae071b064d58991aea67dd529c23714bd75ef2
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-11 09:30:07 +0700

    Completing Subchapter 1
    Added examples for type() and isinstance() functions, including error handling and type checking.

--- 01-preparation/4. Python Basics - freeCodeCamp/01. Understanding Variables and Data Types/01. Understanding Variables and Data Types.py
+++ 01-preparation/4. Python Basics - freeCodeCamp/01. Understanding Variables and Data Types/01. Understanding Variables and Data Types.py
@@ -50,7 +50,50 @@ print('Float:', my_float_var) # Float 4.5
 
 # 4. String (characters enclosed in single or double quotation marks)
 my_string_var = 'hello'
+print('String:', my_string_var) # String: hello
 
 # 5. Boolean (True or False)
 my_boolean_var = True
 print('Boolean:', my_boolean_var) # Boolean: True
+
+# -- D. How Do the type() and isinstance() Functions Work? --
+# -- 1. type() function --
+developer = 'Devin'
+print(type(developer)) # <class 'str'> means String
+
+# Error Example
+"""
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: type() takes 1 or 3 arguments
+"""
+
+# Complete data types
+my_integer_var = 10
+print(type(my_integer_var)) # <class 'int'>
+
+my_float_var = 4.50
+print(type(my_float_var)) # <class 'float'>
+
+my_string_var = 'hello'
+print(type(my_string_var)) # <class 'str'>
+
+my_boolean_var = True
+print(type(my_boolean_var)) # <class 'bool'>
+
+# -- 2. isinstance() function
+# Error when trying to perform mathematical operations
+account_balance = '12'
+account_balance / 2
+
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+#  See if the data type is an integer
+account_balance = '12'
+isinstance(account_balance, int) # False
+
+# Check for multiple types at once
+account_balance = 12
+isinstance(account_balance, (int, float)) # True
