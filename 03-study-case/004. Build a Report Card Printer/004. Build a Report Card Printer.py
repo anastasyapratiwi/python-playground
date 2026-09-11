b415ae9c30593b46937673a1f0292258c8cdc71a
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-11 11:09:17 +0700

    Completed the Report Card Printer workshop
    My first study case at freeCodeCamp is done!

---
+++ 03-study-case/004. Build a Report Card Printer/004. Build a Report Card Printer.py
@@ -0,0 +1,89 @@
+"""
+Python Certification freeCodeCamp
+Chapter: Python Basics
+Sub-Chapter: Build a Report Card Printer (Workshop)
+"""
+
+# -- A. Create a variable called name and assign it the string Alice --
+name = "Alice"
+
+# -- B. Print the value of a variable using the print() function --
+name = 'Alice'
+print(name) # Output: Alice
+
+# -- C. Check the type of a value --
+name = 'Alice'
+print(name)
+print(type(name)) # Output: <class 'str'>
+
+# -- D. Add the student's enrollment status using Boolean --
+name = 'Alice'
+print(name)
+print(type(name))
+
+is_student = True
+
+# -- E. Print both enrollment status and its data type --
+name = 'Alice'
+print(name)
+print(type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+# -- F. Make sure the same format for the student's name --
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+# -- G. Add the student's age to the report card data --
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+# -- H. Add the student's score --
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score,int))
+
+# -- I. Check if the score's data type is a float --
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+
+# -- J.  Complete the report card --
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
