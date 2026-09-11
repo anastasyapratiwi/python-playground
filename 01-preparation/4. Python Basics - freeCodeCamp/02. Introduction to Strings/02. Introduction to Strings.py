be8128851311e8567494601d9f99a65a24f1e5e4
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-11 11:34:43 +0700

    Started the new subchapter about strings
    The new thing I learned was the usage of backlash (\) to avoid the error in the same quotation marks! session = 'it\'s a brand new knowledge'

---
+++ 01-preparation/4. Python Basics - freeCodeCamp/02. Introduction to Strings/02. Introduction to Strings.py
@@ -0,0 +1,26 @@
+"""
+Python Certification freeCodeCamp
+Chapter: Python Basics
+Sub-Chapter: Introduction to Strings
+"""
+
+# -- A. What Are Strings and What Is String Immutability? --
+# Example code
+my_str_1 = 'Hello'
+my_str_2 = "World"
+
+# Multi-line string example
+my_str_3 = """Multiline
+string"""
+my_str_4 = '''Another
+multiline
+string'''
+
+# If the strings contains either single or double quotation marks
+# a. Use the opposite kind of quotes
+msg = "It's a sunny day"
+quote = 'She said, "Hello World!"'
+
+# b. Use backlash (\) for the same quotation marks
+msg = 'It\'s a sunny day'
+quote = "She said, \"Hello!\""
