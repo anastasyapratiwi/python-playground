ef4b9da181dab3f2b0d971eca8adf122b97f2bc3
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-14 10:43:26 +0700

    Complete Subsection "What Are Strings and What Is String Immutability?"

--- 01-preparation/4. Python Basics - freeCodeCamp/02. Introduction to Strings/02. Introduction to Strings.py
+++ 01-preparation/4. Python Basics - freeCodeCamp/02. Introduction to Strings/02. Introduction to Strings.py
@@ -24,3 +24,36 @@ quote = 'She said, "Hello World!"'
 # b. Use backlash (\) for the same quotation marks
 msg = 'It\'s a sunny day'
 quote = "She said, \"Hello!\""
+
+# in (check if a string contains one or more characters)
+my_str = 'Hello world'
+
+print('Hello' in my_str) # True
+print('hey' in my_str) # False
+print('hi' in my_str) # False
+print('e' in my_str) # True
+print('f' in my_str) # False
+
+# len(): Get the length of a string
+my_str = 'Hello world'
+print(len(my_str)) # 11
+
+# []: Access a character by its index
+my_str = "Hello world"
+print(my_str[0]) # H
+print(my_str[6]) # w
+
+# Negative indexing (from the last character)
+my_str = 'Hello world'
+print(my_str[-1]) # d
+print(my_str[-2]) # 1
+
+# Assign a different string to a variable (immutable)
+greeting = 'hi'
+greeting = 'hello'
+print(greeting) # hello
+
+# Don't direct modification of a string
+greeting = 'hi'
+greeting[0] = 'H' # TypeError: 'str' object does not support item assignment
+
