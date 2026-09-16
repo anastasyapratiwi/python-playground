d684f39740d5ef23178f9ff5aae7ba196f97b903
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-16 11:31:05 +0700

    Enhance Python script with integer operation examples
    Added examples for integer operations including addition, subtraction, multiplication, and division.

--- 01-preparation/4. Python Basics - freeCodeCamp/03. Numbers and Mathematical Operations/03. Numbers and Mathematical Operations.py
+++ 01-preparation/4. Python Basics - freeCodeCamp/03. Numbers and Mathematical Operations/03. Numbers and Mathematical Operations.py
@@ -1,5 +1,45 @@
 """
 Python Certification freeCodeCamp
 Chapter: Python Basics
-Sub-Chapter: Numbers and Mathematical Operations
+Subchapter: Numbers and Mathematical Operations
 """
+
+# -- A. How Do You Work With Integers and Floating Point Numbers? --
+# -- 1. With Integers --
+# Example Code
+my_int_1 = 56
+my_int_2 = -4
+print(type(my_int_1)) # <class 'int'>
+print(type(my_int_2)) # <class 'int'>
+
+# - a. Addition operation with integers -
+my_int_1 = 56
+my_int_2 = 12
+sum_ints = my_int1 + my_int_2
+print('Integer Addition:', sum_ints) # Integer Addition: 68
+
+# - b. Substraction with integers -
+my_int_1 = 56
+my_int_2 = 12
+
+# Substraction
+diff_ints = my_int_1 - my_int_2
+print('Integer Substraction:', diff_ints) # Integer Substraction: 44
+
+# - c. Multiplication operation with integers -
+my_int_1 = 12
+my_int_2 = 4
+
+# Multiplication
+product_ints = my_int_1 * my_int_2
+print('Integer Multiplication:', product_ints) # Integer Multiplication: 48
+
+# - d. Division operation with integers -
+my_int_1 = 56
+my_int_2 = 12
+
+# Division
+div_ints = my_int_1 / my_int_2
+print('Division:', div_ints) # Division: 4.666666666666667
+
+# -- 2. With Floats --
