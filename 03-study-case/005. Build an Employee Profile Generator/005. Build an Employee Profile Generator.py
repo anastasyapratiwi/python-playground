aeb3b57bb919dcaddbf8537fcd8e2ee036a40ef9
Anastasya Pratiwi <anastasyapratiwi.work@gmail.com>
2026-09-16 10:59:41 +0700

    Complete Workshop "Build an Employee Profile Generator"
    This 18-part workshop is complete by adding multiple steps to build an employee profile generator, including personal details, address, age, experience, position, salary, and employee code extraction.

--- 03-study-case/005. Build an Employee Profile Generator/005. Build an Employee Profile Generator.py
+++ 03-study-case/005. Build an Employee Profile Generator/005. Build an Employee Profile Generator.py
@@ -4,3 +4,227 @@ Chapter: Python Basics
 Sub-Chapter: Build an Employee Profile Generator (Workshop)
 """
 
+# -- 1. Step 1 --
+first_name = 'John'
+last_name = 'Doe'
+print(first_name)
+print(last_name)
+
+# -- 2. Step 2 --
+first_name = 'John'
+last_name = 'Doe'
+print(first_name)
+print(last_name)
+full_name = first_name + last_name
+print(full_name)
+
+# -- 3. Step 3 --
+first_name = 'John'
+last_name = 'Doe'
+print(first_name)
+print(last_name)
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+# -- 4. Step 4 --
+first_name = 'John'
+last_name = 'Doe'
+print(first_name)
+print(last_name)
+full_name = first_name + ' ' + last_name
+print(full_name)
+address = '123 Main Street'
+print(address)
+
+# -- 5. Step 5 --
+first_name = 'John'
+last_name = 'Doe'
+print(first_name)
+print(last_name)
+full_name = first_name + ' ' + last_name
+print(full_name)
+address = '123 Main Street'  
+address += ', Apartment 4B'
+print(address) 
+
+# -- 6. Step 6 --
+first_name = 'John'  
+last_name = 'Doe'  
+full_name = first_name + ' ' + last_name  
+address = '123 Main Street'  
+address += ', Apartment 4B'  
+
+# -- 7. Step 7 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+
+# -- 8. Step 8 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is '
+
+# -- 9. Step 9 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + employee_age
+
+# -- 10. Step 10 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age)
+
+# -- 11. Step 11 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+
+# -- 12. Step 12 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+
+# -- 13. Step 13 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+employee_card = f'Employee: {full_name}'
+
+# -- 14. Step 14 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+employee_card = f'Employee: {full_name} | Age: {employee_age}' 
+
+# -- 15. Step 15 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+
+# -- 16. Step 16 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+
+employee_code = 'DEV-2026-JD-001'
+department = employee_code[0:3]
+print(department)
+
+# -- 17. Step 17 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+employee_code = 'DEV-2026-JD-001'
+department = employee_code[0:3]
+print(department) 
+year_code = employee_code[4:8]
+initials = employee_code[9:11]
+print(year_code)
+print(initials)
+
+# -- 18. Step 18 --
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = 'Experience: ' + str(experience_years) + ' years'
+print(experience_info)
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+employee_code = 'DEV-2026-JD-001'
+department = employee_code[0:3]
+print(department)
+year_code = employee_code[4:8]
+print(year_code)
+initials = employee_code[9:11]
+print(initials)
+last_three = employee_code[-3:]
+print(last_three)
